3176f37e4658775c8ae7526d4994a5954673456b
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-11-07 13:56:56 -0500

    import home and login pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
