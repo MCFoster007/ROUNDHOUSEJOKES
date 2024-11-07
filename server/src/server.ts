8497900cf039948f07b6cbb352d99da67ba53f9e
Sebastian C <sacasadevall@gmail.com>
2024-11-07 12:00:36 -0500

    fixed code for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-// const forceDatabaseRefresh = false;
+const forceDatabaseRefresh = false;
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,21 +6,17 @@ import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 // Serves static files in the entire client's dist folder
-// app.use(express.static(path.resolve(__dirname, '../client')));
+app.use(express.static(path.resolve('../client/dist')));
 
-// Uncomment once client folder is resolved
-// app.get('/', (_, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/index.html'));
-// });
+
 
 app.get('/', (_, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
@@ -30,14 +26,10 @@ app.get('/', (_, res) => {
 app.use(express.json());
 app.use(routes);
 
-// sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-//   });
-// });
-
-sequelize.sync().then(() => {
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
+
+
