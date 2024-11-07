6b5ce4b2d7c90046dbf95e0ce388f4777891a17f
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-07 15:28:43 -0500

    added id to ID on likedjokes

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,10 @@
 import sequelize from '../config/connection.js';
 import { UserFactory } from './user.js';
+import {JokeFactory} from './likedJoke.js';
 
 const User = UserFactory(sequelize);
+const Joke = JokeFactory(sequelize);
+User.hasMany(Joke, {foreignKey:'userID', onDelete:'CASCADE'});
+Joke.belongsTo(User, {foreignKey:'userID'});
 
-export { User };
+export { User, Joke };
