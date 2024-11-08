ef059eda6e5d5e18ed8322e5b89237e6fa0198c4
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-07 22:24:39 -0500

    added code to jokes routes

--- server/src/routes/jokes-routes.ts
+++ server/src/routes/jokes-routes.ts
@@ -1,4 +1,6 @@
 import { Router, type Request, type Response } from "express";
+import {Joke} from '../models/likedJoke.js';
+
 const retrieveJokes = async (_req: Request, res: Response) => {
   try {
     const response = await fetch("https://v2.jokeapi.dev/joke/Programming", {
@@ -18,20 +20,24 @@ const retrieveJokes = async (_req: Request, res: Response) => {
     return {error: err};
   }
 };
-// const retrieveLiked = async (_req: Request, res: Response) => {
-//   try {
-//     const response = await fetch ()
-//     // TODO: find all liked jokes by user with ID, user_ID
+const retrieveLiked = async (_req: Request, res: Response) => {
+  try {
+    const userID = _req.params.userID;
+    const likedJokes =await Joke.findAll ({where: {userID}});
+    return res.status(200).json(likedJokes);
+    // TODO: find all liked jokes by user with ID, user_ID
 
-//     return res.status(418).send ('to be implemented');
-//   } catch (err) {
-//     console.log("Error from data retrieval:", err);
-//     return {error: err};
-//   }
-
-// };
+    // return res.status(418).send ('to be implemented');
+  } catch (err) {
+    console.log("Error from data retrieval:", err);
+    return res.status(500).json ({error: 'failed to retrieved liked jokes'});
+  }
+  return retrieveLiked;
+};
 const likeJoke = async (_req: Request, res: Response) => {
   try {
+
+    
     // TODO: add joke to the DB with user_ID
     //body must include joke text and user id 
 
@@ -47,4 +53,8 @@ const router = Router();
 router.get("/Jokes", retrieveJokes);
 // router.get('/liked/:user_ID', retrieveLiked);
 router.post('/like', likeJoke)
+
+
 export default router;
+
+
